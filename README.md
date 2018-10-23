30cd6171dcdd6305b247769b054f83076396fde7
Romeo Bahoumda <bbb219@lehigh.edu>
2018-10-23 10:29:45 -0400

    First commit, and Maven setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Source-Web
+
+This repository contains files regarding the webpage of the Student Organization and Resource Center 
+at the source.
+
+The tools used are:
+- Java
+- Maven 
+- Heroku
---
+++ source-web/src/main/java/com/lehigh/source/App.java
@@ -0,0 +1,13 @@
+package com.lehigh.source;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
