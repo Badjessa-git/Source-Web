c19c74e2dd4b4d7e3e68497410b3050470e9a932
Romeo Bahoumda <bbb219@lehigh.edu>
2018-11-12 15:48:07 -0500

    Added some code to the front end

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -18,6 +18,10 @@ npm update
 cp node_modules/jquery/dist/jquery.min.js $TARGETFOLDER/$WEBFOLDERNAME
 cp -r ./js/ $TARGETFOLDER/$WEBFOLDERNAME/js/
 cp -r ./css/ $TARGETFOLDER/$WEBFOLDERNAME/css/
+cp node_modules/jquery/dist/jquery.min.js $TARGETFOLDER/$WEBFOLDERNAME
+cp node_modules/bootstrap/dist/js/bootstrap.min.js $TARGETFOLDER/$WEBFOLDERNAME
+cp node_modules/bootstrap/dist/css/bootstrap.min.css $TARGETFOLDER/$WEBFOLDERNAME
+cp -R node_modules/bootstrap/dist/fonts $TARGETFOLDER/$WEBFOLDERNAME
 
 #put our index.html into the resources of backend
 cp index.html $TARGETFOLDER/$WEBFOLDERNAME
